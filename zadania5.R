7f4c5b07fd105829487835c167f3444c4db2da1e
Czarnodziura <37172512+Czarnodziura@users.noreply.github.com>
2018-04-19 07:18:14 +0200

    zadania5 (poprawione)

--- zadania5.R
+++ zadania5.R
@@ -8,7 +8,7 @@ View(movies)
 
 
 #Zad 5-2
-filter(movies, year == 2005)
+filter(movies, year == 2005, Comedy == 1)
   
 
 #Zad 5-3
@@ -16,10 +16,15 @@ movies %>% select(title, year, budget) %>% arrange(desc(budget))
 
 
 #Zad 5-4
-movies %>% select(Animation, year) %>%  filter(year == 1990)
+filter(movies, Animation == 1, year >=1990 & year < 2000) %>%
+  arrange(desc(rating))
 
 #Zad 5-5
-movies %>% select(Drama, length) %>% arrange(desc(length))
+dramaty <- filter(movies, Drama == 1)
+arrange(dramaty, desc(length))
 
+#Zad 5-6
+mpaa_rating <- group_by(movies, mpaa) %>%
+  summarise (srednia = mean(rating), odchylenie = mad(rating))
 
 
